3e890e3701a52a8a29cc31d0a7d5536f145b2357
Thomas Jongstra <jongstra@gmail.com>
2024-08-10 02:41:45 +0200

    Adds stub for technique filter component.

---
+++ frontend/src/components/TechniqueFilter.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  import { tacticsStore } from '@/stores/tactics';
+  const store = tacticsStore()
+
+  // store.applyTechniqueFilter();
+</script>
+
+<template>
+
+      <div v-if="store.domain === 'enterprise-attack'">
+        <p>Enterprise</p>
+      </div>
+
+      <div v-else-if="store.domain === 'mobile-attack'">
+        <p>Mobile</p>
+      </div>
+
+      <div v-else-if="store.domain === 'ics-attack'">
+        <p>ICS</p>
+      </div>
+
+</template>
+
+<!-- 
+TODO
+
+- [x] Data sources list maken met Python.
+- [x] Data sources list serveren via Flask.
+- [x] Data sources list ingesten in Pinia.
+- [ ] Data sources list in deze component tonen. Denk aan een MultiSelect-achtige component (denk aan https://primevue.org/multiselect/)
+- [ ] ATT&CK matrix filteren obv 
+
+-->
