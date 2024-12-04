67e10e4bcec8edf8a4b5e16921a7f86feb4e091e
Thomas Jongstra <jongstra@gmail.com>
2024-12-04 05:48:29 +0100

    Remove console.log statement.

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -44,8 +44,6 @@ const updateTooltipPosition = () => {
     // Adjust tooltip position to be relative to the document
     tooltipPosition.value.top = buttonRect.bottom + scrollTop - 270; // Position below the button
     tooltipPosition.value.left = buttonRect.left + scrollLeft; // Position the tooltip horizontally
-
-    console.log(tooltipPosition.value.top, tooltipPosition.value.left);
   }
 };
 
