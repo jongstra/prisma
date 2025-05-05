5d5b448d0017f11d5e5dea000e7f4b49164ec5b8
Thomas Jongstra <jongstra@gmail.com>
2025-05-05 13:05:07 +0200

    Add a basic MaGMa heatmap button tooltip, showing weight, visibility, implementation and effectiveness values.

--- frontend/src/components/magma/HeatmapButton.vue
+++ frontend/src/components/magma/HeatmapButton.vue
@@ -1,58 +1,122 @@
 <script setup lang="ts">
 import { magmaStore } from '@/stores/magma';
+import { ref } from 'vue';
 
 const props = defineProps(['technique']);
 const magma = magmaStore();
 
+// Tooltip variables.
+let showTooltipBool = ref(false);
+const buttonRef = ref<HTMLElement | null>(null);
+let tooltipPosition = ref({ top: 0, left: 0 });
+
+// Gather the use cases that are related to the technique.
+const relatedUseCases = magma.getUseCasesByAttackTechniqueId(props.technique.external_id);
+
+// Pre-compute some values using the related use cases.
+const averageWeight = relatedUseCases.reduce((sum, useCase) => sum + useCase.weight/100, 0) / relatedUseCases.length;
+const averageVisibility = relatedUseCases.reduce((sum, useCase) => sum + useCase.visibility/100, 0) / relatedUseCases.length;
+const averageImplementation = relatedUseCases.reduce((sum, useCase) => sum + useCase.implementation/100, 0) / relatedUseCases.length;
+const averageEffectiveness = relatedUseCases.reduce((sum, useCase) => sum + useCase.effectiveness/100, 0) / relatedUseCases.length;
+
+
+// Function to calculate the cumulative scroll positions of all ancestors.
+function calculateScroll(e) {
+  if (e && e.parentNode) {
+    const [scrollTop, scrollLeft] = calculateScroll(e.parentNode);
+    return [(e.scrollTop || 0) + scrollTop, (e.scrollLeft || 0) + scrollLeft];
+  } else {
+    return [0, 0];
+  }
+};
+
+// Update the tooltip position relative to the document.
+function updateTooltipPosition() {
+  const buttonRect = buttonRef.value.getBoundingClientRect();
+  const [scrollTop, scrollLeft] = calculateScroll(buttonRef.value);
+
+  // Adjust tooltip position to be relative to the document
+  tooltipPosition.value.top = buttonRect.bottom + scrollTop - 192; // Position below the button
+  tooltipPosition.value.left = buttonRect.left + scrollLeft + 10; // Position the tooltip horizontally
+};
+
+
+function showTooltip() {
+  updateTooltipPosition();
+  showTooltipBool.value = true;
+}
+
+function hideTooltip() {
+  showTooltipBool.value = false;
+}
+
 
 function getBackgroundColor() {
-  const relatedUseCases = magma.getUseCasesByAttackTechniqueId(props.technique.external_id);
 
-  console.log(relatedUseCases);
   // Heatmap style: weight
   if (magma.heatmapVisibility && magma.heatmapImplementation && magma.heatmapEffectiveness) {
-    const averageWeight = relatedUseCases.reduce((sum, usecase) => sum + usecase.weight/100, 0) / relatedUseCases.length;
       return `rgba(0, 255, 0, ${averageWeight})`;
   }
 
   // Heatmap style: visibility * implementation
   if (magma.heatmapVisibility && magma.heatmapImplementation) {
-    const averageVisImp = relatedUseCases.reduce((sum, usecase) => sum + (usecase.visibility/100 * usecase.implementation/100), 0) / relatedUseCases.length;
+    const averageVisImp = relatedUseCases.reduce((sum, useCase) => sum + (useCase.visibility/100 * useCase.implementation/100), 0) / relatedUseCases.length;
     return `rgba(0, 255, 0, ${averageVisImp})`;
   }
 
   // Heatmap style: implementation * effectiveness
   if (magma.heatmapImplementation && magma.heatmapEffectiveness) {
-    const averageImpEff = relatedUseCases.reduce((sum, usecase) => sum + (usecase.implementation/100 * usecase.effectiveness/100), 0) / relatedUseCases.length;
+    const averageImpEff = relatedUseCases.reduce((sum, useCase) => sum + (useCase.implementation/100 * useCase.effectiveness/100), 0) / relatedUseCases.length;
     return `rgba(0, 255, 0, ${averageImpEff})`;
   }
 
   // Heatmap style: visibility
   if (magma.heatmapVisibility) {
-    const averageVisibility = relatedUseCases.reduce((sum, usecase) => sum + usecase.visibility/100, 0) / relatedUseCases.length;
     return `rgba(0, 255, 0, ${averageVisibility})`;
   }
 
   // Heatmap style: implementation
   if (magma.heatmapImplementation) {
-    const averageImplementation = relatedUseCases.reduce((sum, usecase) => sum + usecase.implementation/100, 0) / relatedUseCases.length;
     return `rgba(0, 255, 0, ${averageImplementation})`;
   }
 
   // Heatmap style: effectiveness
   if (magma.heatmapEffectiveness) {
-    const averageEffectiveness = relatedUseCases.reduce((sum, usecase) => sum + usecase.effectiveness/100, 0) / relatedUseCases.length;
     return `rgba(0, 255, 0, ${averageEffectiveness})`;
   }
 }
+
+
+
+function getTooltipText() {
+
+  const tooltipText = `<a href='https://attack.mitre.org/techniques/${props.technique.external_id}/' target="_blank">${props.technique.name}</a> (${props.technique.external_id})\n
+  Weight: ${((averageWeight || 0) * 100).toFixed(0)}%
+  Visibility: ${((averageVisibility || 0) * 100).toFixed(0)}%
+  Implementation: ${((averageImplementation || 0) * 100).toFixed(0)}%
+  Effectiveness: ${((averageEffectiveness || 0) * 100).toFixed(0)}%`
+
+  return tooltipText
+};
 
 </script>
 
 <template>
   <button
+    ref="buttonRef"
     :style="{'background-color': getBackgroundColor()}"
+    @mouseover="showTooltip"
+    @mouseleave="hideTooltip"
   >
     <span class="buttontext">{{ technique.name }}</span>
+
+    <div v-if="showTooltipBool" class="tooltip"
+       :style="{ top: `${tooltipPosition.top}px`, left: `${tooltipPosition.left}px` }"
+    >
+      <div v-html="getTooltipText()"></div>
+      <!-- Tooltip Test -->
+    </div>
+    
   </button>
 </template>
 
@@ -77,6 +141,20 @@ button {
   display: block;
   text-overflow: ellipsis;
   text-align: center;
+}
+
+.tooltip {
+  position: fixed; /* Use fixed positioning for better control */
+  background-color: rgba(93, 125, 152, 0.9);
+  color: white;
+  border: 1px solid black;
+  padding: 4px 4px;
+  border-radius: 4px;
+  font-size: 11px;
+  z-index: 1002; /* Ensure tooltip is always on top */
+  width: 240px;
+  text-align: left;
+  white-space: pre-line;
 }
 
 </style>
