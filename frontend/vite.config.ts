1ca236925439f2c3fd25d895af8058dfdb9f7362
Thomas Jongstra <jongstra@gmail.com>
2025-12-20 01:47:32 +0100

    Few updates to code and MITRE definitions.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,12 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
+// Determine if we should load VueDevTools
+const loadVueDevTools = process.env.OMIT_VUE_DEVTOOLS !== 'true'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // vueJsx(),
-    // VueDevTools(),
+    vueJsx(),
+    ...(loadVueDevTools ? [VueDevTools()] : [])
   ],
   resolve: {
     alias: {
